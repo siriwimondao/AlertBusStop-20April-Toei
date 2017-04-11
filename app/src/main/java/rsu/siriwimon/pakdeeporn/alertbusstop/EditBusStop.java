ca954cf5a6927d2fb60c83100e1ae6d394bc6884
masterUNG <phrombutr@gmail.com>
2017-04-11 15:02:34 +0700

    finish

---
+++ app/src/main/java/rsu/siriwimon/pakdeeporn/alertbusstop/EditBusStop.java
@@ -0,0 +1,13 @@
+package rsu.siriwimon.pakdeeporn.alertbusstop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditBusStop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_bus_stop);
+    }
+}
